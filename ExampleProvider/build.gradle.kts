31c6197578caee66091647da99142d6220b460fd
YoshiCantScript <149406088+YoshiCantScript@users.noreply.github.com>
2024-02-03 00:11:49 +0100

    Update build.gradle.kts

--- ExampleProvider/build.gradle.kts
+++ ExampleProvider/build.gradle.kts
@@ -1,24 +1,25 @@
-// use an integer for version numbers
-version = 1
+version = 7
 
 
 cloudstream {
+    language = "it"
     // All of these properties are optional, you can safely remove them
 
-    description = "Lorem Ipsum"
-    authors = listOf("Cloudburst")
+    // description = "Lorem Ipsum"
+    authors = listOf("Adippe","Forthe")
 
     /**
-    * Status int as the following:
-    * 0: Down
-    * 1: Ok
-    * 2: Slow
-    * 3: Beta only
-    * */
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+     * */
     status = 1 // will be 3 if unspecified
+    tvTypes = listOf(
+        "TvSeries",
+        "Movie",
+    )
 
-    // List of video source types. Users are able to filter for extensions in a given category.
-    // You can find a list of avaliable types here:
-    // https://recloudstream.github.io/cloudstream/html/app/com.lagradost.cloudstream3/-tv-type/index.html
-    tvTypes = listOf("Others")
+    iconUrl = "https://www.google.com/s2/favicons?domain=streamingcommunity.online&sz=%size%"
 }
